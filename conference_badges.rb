c40de367f8d203574b81f9bc933809658d325e65
Conor Hamilton <conhamil19@gmail.com>
2018-05-14 10:02:37 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,7 +3,9 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(name_array)
+  new_array = []
   name_array.each do |name|
-   return "Hello, my name is #{name}."
+    new_array << "Hello, my name is #{name}."
   end
+  new_array
 end
